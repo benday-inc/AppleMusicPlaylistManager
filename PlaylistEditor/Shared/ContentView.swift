90cad8a7e08c3c715afe86916ed5ce14d4ddf6f0
benday <benday@benday.com>
2021-12-27 09:24:49 -0500

    switched songs tab to be first

--- PlaylistEditor/Shared/ContentView.swift
+++ PlaylistEditor/Shared/ContentView.swift
@@ -13,13 +13,14 @@ struct ContentView: View {
     
     var body: some View {
         TabView {
+            SongsView(musicAuthorizationStatus: .constant(.notDetermined), items: []).tabItem {
+                Label("Songs", systemImage: "square.and.pencil")
+            }.tag(0)
             PlaylistsView(musicAuthorizationStatus: .constant(.notDetermined), _playlists: [])
                 .tabItem {
                 Label("Playlists", systemImage: "music.note.list")
-            }.tag(0)
-            SongsView(musicAuthorizationStatus: .constant(.notDetermined), items: []).tabItem {
-                Label("Songs", systemImage: "square.and.pencil")
             }.tag(1)
+            
         }
 
     }

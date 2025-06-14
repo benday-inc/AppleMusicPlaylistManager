f7b43b3b688668085655d157530894ca7f7e7847
benday <benday@benday.com>
2025-06-14 18:10:41 -0400

    moving modern version of app with carplay to this repo

--- PlaylistEditor/CarPlaySceneDelegate.swift
+++ PlaylistEditor/CarPlaySceneDelegate.swift
@@ -9,16 +9,18 @@ import Foundation
 import CarPlay
 
 class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
+    
+    var interfaceController: CPInterfaceController?
+    
+    // CarPlay connected
     func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
-                                  didConnect interfaceController: CPInterfaceController,
-                                  to window: CPWindow) {
-        let listItem = CPListItem(text: "Hello CarPlay", detailText: "From your app")
-        listItem.handler = { _, completion in
-            print("Tapped!")
-            completion()
-        }
-
-        let listTemplate = CPListTemplate(title: "Demo List", sections: [CPListSection(items: [listItem])])
+                                  didConnect interfaceController: CPInterfaceController) {
+        
+        self.interfaceController = interfaceController
+        
+        //setInformationTemplate()
+        
+        let listTemplate: CPListTemplate = CarPlayHelloWorld().template
         interfaceController.setRootTemplate(listTemplate, animated: true) { completed, error in
             if let error = error {
                 print("Failed to set root template: \(error)")
@@ -26,5 +28,26 @@ class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
                 print("Successfully set root template")
             }
         }
+    }
+    
+    // CarPlay disconnected
+    private func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
+                                  didDisconnect interfaceController: CPInterfaceController) {
+        self.interfaceController = nil
+    }
+    
+}
+
+class CarPlayHelloWorld {
+    var template: CPListTemplate {
+        return CPListTemplate(title: "Hello world", sections: [self.section])
+    }
+    
+    var items: [CPListItem] {
+        return [CPListItem(text:"Hello world", detailText: "The world of CarPlay", image: UIImage(systemName: "globe"))]
+    }
+    
+    private var section: CPListSection {
+        return CPListSection(items: items)
     }
 }

cee7f5d6783f512e8c7b14a52b61cfddc51827ec
benday <benday@benday.com>
2025-07-16 15:02:18 -0400

    Got multi select working for add artist

--- Playlists/Playlists/AddArtistView.swift
+++ Playlists/Playlists/AddArtistView.swift
@@ -11,7 +11,8 @@ import MediaPlayer
 
 struct AddArtistView: View {
     @State private var newItemName = ""
-    @State private var selectedItem: String?
+    @State private var selectedItems: Set<String> = []
+    @State private var editMode = EditMode.active
     @State private var searchText = ""
     @Binding var isPresented: Bool
     @ObservedObject var category: CategoryViewModel
@@ -20,13 +21,10 @@ struct AddArtistView: View {
     
     var body: some View {
         NavigationStack {
-            List($matchingItems) { $item in
-                Button(action: {
-                    selectedItem = $item.wrappedValue.value
-                }) {
-                    Text(item.value)
-                }.buttonStyle(.plain)
+            List(matchingItems, id: \.value, selection: $selectedItems) { item in
+                Text(item.value)
             }
+            .environment(\.editMode, $editMode)
             .searchable(text: $searchText, prompt: "Search for artists")
             .onChange(of: searchText) { oldValue, newValue in
                 let trimmed = newValue.trimmingCharacters(in: .whitespaces)
@@ -40,29 +38,33 @@ struct AddArtistView: View {
                     }
                 }
                 ToolbarItemGroup(placement: .bottomBar) {
-                    if (selectedItem == nil || selectedItem?.isEmpty == true) {
-                        Text("Select a artist above")
-                            .foregroundStyle(.secondary)
-                            .padding()
-                    } else {
-                        Text("Selected artist: \(selectedItem!)")
-                            .padding()
-                    }
-                    
-                    Button("Add") {
-                        if let selectedItem {
-                            let trimmed = selectedItem.trimmingCharacters(in: .whitespacesAndNewlines)
-                            if !trimmed.isEmpty && !category.artists.contains(trimmed) {
-                                category.artists.append(trimmed)
-                            }
+                    HStack {
+                        if selectedItems.isEmpty {
+                            Text("Select artist(s) above")
+                                .foregroundStyle(.secondary)
+                                .padding()
+                            Spacer()
+                        } else {
+                            let text = getSelectedText()
+                            
+                            
+                            Text("Selected: \(text)")
+                                .padding()
+                            Spacer()
+                        }
+                        
+                        Button("Add") {
+                            let trimmedItems = selectedItems.map { $0.trimmingCharacters(in: .whitespacesAndNewlines) }
+                            let newItems = trimmedItems.filter { !$0.isEmpty && !category.artists.contains($0) }
+                            category.artists.append(contentsOf: newItems)
                             newItemName = ""
                             isPresented = false
                         }
-                    }
-                    .buttonStyle(.borderedProminent)
-                    .disabled(
-                        selectedItem == nil || selectedItem?.isEmpty == true
-                    )
+                        .buttonStyle(.borderedProminent)
+                        .disabled(selectedItems.isEmpty)
+                        .frame(minWidth: 80)
+                    }                    
+                    
                 }
                 
             }
@@ -74,6 +76,44 @@ struct AddArtistView: View {
             .navigationTitle("Add artist")
         }
         
+    }
+    
+    private func getSelectedText() -> String {
+        let joined = selectedItems.joined(separator: ", ")
+
+        if (UIDevice.current.userInterfaceIdiom == .phone) {
+            let orientation = UIDevice.current.orientation
+            
+            let maxChars : Int
+            
+            if (orientation.isLandscape == true) {
+                maxChars = 60
+            }
+            else
+            {
+                maxChars = 15
+            }
+                
+            let text = joined.count > maxChars
+                ? joined.prefix(maxChars) + "…"
+                : joined
+
+            return text
+        }
+        else {
+            let maxChars = 80
+            
+            if (joined.count > maxChars) {
+                return joined.prefix(maxChars) + "…"
+            }
+            else {
+                return joined
+            }
+        }
+        
+
+        
+                
     }
     
     private func updateMatching(for query: String) {
@@ -95,7 +135,16 @@ struct AddArtistView: View {
 }
 
 #Preview("with matching artists") {
-    @Previewable @State var temp = [IdentifiableString(value: "Rock"), IdentifiableString(value: "Pop"), IdentifiableString(value: "Jazz")]
+    @Previewable @State var temp = [
+        IdentifiableString(value: "Bob Bobbenbobber"),
+        IdentifiableString(value: "Bobbenbobber"),
+        IdentifiableString(value: "Sneeze"),
+        IdentifiableString(value: "Pumpkin"),
+        IdentifiableString(value: "Big Long Name That is Super Long 1"),
+        IdentifiableString(value: "Big Long Name That is Super Long 2"),
+        IdentifiableString(value: "Big Long Name That is Super Long 3"),
+        IdentifiableString(value: "Big Long Name That is Super Long 4"),
+        IdentifiableString(value: "Junk n Stuff")]
     var category = Category()
     category.name = "Test"
     let categoryVM = CategoryViewModel()

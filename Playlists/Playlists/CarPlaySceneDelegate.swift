699bc80b0297ae7eb9a9de97ef163e2a43b82670
benday <benday@benday.com>
2025-07-19 12:15:49 -0400

    changing the icon for currently playing category

--- Playlists/Playlists/CarPlaySceneDelegate.swift
+++ Playlists/Playlists/CarPlaySceneDelegate.swift
@@ -4,35 +4,19 @@ import UIKit
 class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
     
     var interfaceController: CPInterfaceController?
+    var playingCategoryId: UUID?
     
     func templateApplicationScene(
         _ templateApplicationScene: CPTemplateApplicationScene,
         didConnect interfaceController: CPInterfaceController) {
             
+            self.interfaceController = interfaceController
             let dataStore = PlaylistDataStore()
             Task {
                 await dataStore.load()
                 guard dataStore.isLoaded else { return }
                 
-                let buttons = dataStore.categories.map { category in
-                    let symbol = UIImage(systemName: "music.note") ?? UIImage()
-                    return CPGridButton(titleVariants: [category.name], image: symbol) { [weak self] _ in
-                        guard let self = self else { return }
-//                        let detail = self.makeDetailList(for: category)
-//                        self.interfaceController?.presentTemplate(detail, animated: true, completion: nil)
-                        
-                        let viewModel = SongsViewModel(category: category, storage: dataStore)
-                        
-                        viewModel.play()
-                    }
-                }
-                
-                let grid = CPGridTemplate(title: "Categories", gridButtons: buttons)
-                do {
-                    try await interfaceController.setRootTemplate(grid, animated: true)
-                } catch {
-                    print("Failed to set root template: \(error)")
-                }
+                await reloadCategoryGrid(with: dataStore)
             }
         }
     
@@ -51,5 +35,29 @@ class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
     private func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
                                           didDisconnect interfaceController: CPInterfaceController) {
         self.interfaceController = nil
+    }
+
+    func reloadCategoryGrid(with dataStore: PlaylistDataStore) async {
+        let buttons = dataStore.categories.map { category in
+            let config = UIImage.SymbolConfiguration(pointSize: 36, weight: .semibold, scale: .large)
+            let isPlaying = category.id == self.playingCategoryId
+            let symbolName = isPlaying ? "music.note.list" : "music.note"
+            let symbol = UIImage(systemName: symbolName, withConfiguration: config)?.withRenderingMode(.alwaysTemplate) ?? UIImage()
+
+            return CPGridButton(titleVariants: [category.name], image: symbol) { [weak self] _ in
+                guard let self = self else { return }
+                self.playingCategoryId = category.id
+                let viewModel = SongsViewModel(category: category, storage: dataStore)
+                viewModel.play()
+                Task { await self.reloadCategoryGrid(with: dataStore) }
+            }
+        }
+
+        let grid = CPGridTemplate(title: "Categories", gridButtons: buttons)
+        do {
+            try await interfaceController?.setRootTemplate(grid, animated: true)
+        } catch {
+            print("Failed to update grid template: \(error)")
+        }
     }
 }

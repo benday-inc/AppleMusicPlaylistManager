68cc5bc901db1e2be20b474cb0abecaef6ed679f
benday <benday@benday.com>
2025-07-19 16:35:25 -0400

    Got the clearable text working

--- Playlists/Playlists/ClearableTextField.swift
+++ Playlists/Playlists/ClearableTextField.swift
@@ -11,17 +11,16 @@ import SwiftUI
 struct ClearableTextField: View {
     @Binding var text: String
     var placeholder: String
-
+    
     var body: some View {
         HStack {
             TextField(placeholder, text: $text)
-                if !text.isEmpty {
                 Button(action: { text = "" }) {
                     Image(systemName: "xmark.circle.fill")
                         .foregroundColor(.gray)
                 }
                 .buttonStyle(.plain)
-            }
+                .disabled(text.isEmpty)            
         }
     }
 }
